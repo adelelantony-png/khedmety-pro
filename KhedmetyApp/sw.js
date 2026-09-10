e358edbcbe3aa120894d6613931a646e616356b2
adelelantony-png <adel.elantony@gmail.com>
2026-09-10 14:57:38 +0300

    Add files via upload

---
+++ KhedmetyApp/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'khedmety-offline-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
+    'https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+    'https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js',
+    'https://www.gstatic.com/firebasejs/9.23.0/firebase-database-compat.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
